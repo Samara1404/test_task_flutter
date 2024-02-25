3ac011b4da2d7a4035a69515d7ce317022d37e51
Samara1404 <stambekova1404@gmail.com>
2024-02-25 23:26:08 +0300

    test_task_flutter

---
+++ lib/home_page.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:test_task_flutter/style.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text('Booster', style: AppTextStyle.style1),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Image.asset('/ic2.png', width: 20, height: 20),
+            ),
+          ]),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const Center(
+                  child: Text('ИНФОРМАЦИЯ ОБ АРТИКУЛЕ',
+                      style: AppTextStyle.style1)),
+              const SizedBox(height: 20),
+              TextFormField(
+                
+                decoration: InputDecoration(
+                 filled: true,
+                 fillColor: const Color.fromARGB(255, 240, 240, 240),
+                  border: OutlineInputBorder(
+                   
+                      borderRadius: BorderRadius.circular(15)),
+                    
+                  hintText: 'Номер артикула *',
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                maxLines: 8,
+                decoration: InputDecoration(
+                   filled: true,
+                 fillColor: const Color.fromARGB(255, 240, 240, 240),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    hintText: 'Описание артикула'),
+              ),
+              const SizedBox(height: 8),
+              const Text('МАТЕРИАЛ', style: AppTextStyle.style1),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 390,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        
+                         filled: true,
+                 fillColor: const Color.fromARGB(255, 240, 240, 240),
+                          hintText: "Материал, цвет *",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  const Icon(
+                    Icons.circle_rounded,
+                    size: 56,
+                    color:   Color.fromARGB(255, 240, 240, 240),
+                  ), // TODO:
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 225,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                         filled: true,
+                 fillColor: const Color.fromARGB(255, 240, 240, 240),
+                          hintText: "Кол-во на ед. прод. *",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  SizedBox(
+                    width: 225,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                         filled: true,
+                 fillColor: const Color.fromARGB(255, 240, 240, 240),
+                          hintText: 'Ед. измерения *',
+                          suffixIcon: const Icon(Icons.arrow_drop_down_outlined,
+                              size: 40),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 240, 240, 240), padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 150)),
+                onPressed: () {},child: const Text('+ материал', style: AppTextStyle.style3), ),
+              
+              const SizedBox(height: 8),
+              const Text(
+                'ФУРНИТУРА',
+                style: AppTextStyle.style1,
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 390,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                         filled: true,
+                 fillColor: const Color.fromARGB(255, 240, 240, 240),
+                          hintText: "Фурнитура *",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  const Icon(
+                    Icons.circle_rounded,
+                    size: 56,
+                    color:   Color.fromARGB(255, 240, 240, 240),
+                  ), // TODO:
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 225,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                         filled: true,
+                 fillColor: const Color.fromARGB(255, 240, 240, 240),
+                          hintText: "Кол-во на единицу *",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  SizedBox(
+                    width: 225,
+                    child: TextFormField(
+                      
+                      decoration: InputDecoration(
+                         filled: true,
+                 fillColor: const Color.fromARGB(255, 240, 240, 240),
+                          hintText: 'Ед. измерения *',
+                          suffixIcon: const Icon(Icons.arrow_drop_down_outlined,
+                              size: 40),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+           ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 240, 240, 240), padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 150)),
+                onPressed: () {},child: const Text('+ фурнитура', style: AppTextStyle.style3), ),
+              
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text('Сохранить'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class AppTextStyle{
+  static const TextStyle style1 =
+   TextStyle(fontSize: 24, fontWeight: FontWeight.w700, color: Colors.black);
+    static const TextStyle style2 =
+   TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black);
+   
+    static const TextStyle style3 =
+   TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.white);
+}
